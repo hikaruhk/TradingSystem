fff7b99eb35d6c7f221528eab10b3cfc88f48115
aarl <hktakaoka@live.com>
2021-06-05 01:12:06 -0700

    add a few more tests, stop flacky tests

--- TradingSystemTests/TradingServiceTests.cs
+++ TradingSystemTests/TradingServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using System;
 using System.Threading.Tasks;
@@ -14,8 +15,13 @@ namespace TradingSystemTests
         [Test]
         public async Task ShouldPostATradeWithSuccessfullyAsGTC()
         {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
             var database = new DbContextOptionsBuilder<TradingDbContext>()
                 .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
                 .Options;
 
             using var context = new TradingDbContext(database);
@@ -63,8 +69,13 @@ namespace TradingSystemTests
         [Test]
         public async Task ShouldPostATradeWithoutCompletingAsIOC()
         {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
             var database = new DbContextOptionsBuilder<TradingDbContext>()
                 .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
                 .Options;
 
             using var context = new TradingDbContext(database);
@@ -110,8 +121,13 @@ namespace TradingSystemTests
         [Test]
         public async Task ShouldPostATradePartiallyAsGTC()
         {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
             var database = new DbContextOptionsBuilder<TradingDbContext>()
                 .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
                 .Options;
 
             using var context = new TradingDbContext(database);
@@ -169,6 +185,179 @@ namespace TradingSystemTests
             existing
                 .Should()
                 .BeNull();
+        }
+
+        [Test]
+        public async Task ShouldRetrieveExecutedOrders()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var database = new DbContextOptionsBuilder<TradingDbContext>()
+                .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+
+            using var context = new TradingDbContext(database);
+
+            var existingOrder = Guid.NewGuid().ToString();
+            await context.ExecutedTradeOrders.AddRangeAsync(
+                new ExecutedTradeOrder
+                {
+                    Id = existingOrder,
+                    CreatedDate = DateTime.Now,
+                    OrderType = "GTC",
+                    Price = 10,
+                    Quantity = 100,
+                    SecurityCode = "AAPL",
+                    Side = "SELL"
+                });
+
+            await context.SaveChangesAsync();
+
+            var service = new TradingService(context);
+
+            var orders = service.GetOrders(DateTime.MinValue, DateTime.MaxValue);
+
+            orders
+                .Should()
+                .BeEquivalentTo(new[] { new ExecutedTradeOrder
+                {
+                    Id = existingOrder,
+                    CreatedDate = DateTime.Now,
+                    OrderType = "GTC",
+                    Price = 10,
+                    Quantity = 100,
+                    SecurityCode = "AAPL",
+                    Side = "SELL" 
+                }}, options => options.Excluding(o => o.CreatedDate));
+        }
+
+        [Test]
+        public async Task ShouldNotRetrieveExecutedOrders()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var database = new DbContextOptionsBuilder<TradingDbContext>()
+                .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+
+            using var context = new TradingDbContext(database);
+
+            var existingOrder = Guid.NewGuid().ToString();
+            await context.ExecutedTradeOrders.AddRangeAsync(
+                new ExecutedTradeOrder
+                {
+                    Id = existingOrder,
+                    CreatedDate = DateTime.Now,
+                    OrderType = "GTC",
+                    Price = 10,
+                    Quantity = 100,
+                    SecurityCode = "AAPL",
+                    Side = "SELL"
+                });
+
+            await context.SaveChangesAsync();
+
+            var service = new TradingService(context);
+
+            var orders = service.GetOrders(DateTime.MinValue, DateTime.MinValue);
+
+            orders
+                .Should()
+                .BeEmpty();
+        }
+
+
+
+
+        [Test]
+        public async Task ShouldRetrieveOrders()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var database = new DbContextOptionsBuilder<TradingDbContext>()
+                .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+
+            using var context = new TradingDbContext(database);
+
+            var existingOrder = Guid.NewGuid().ToString();
+            await context.TradeOrders.AddRangeAsync(
+                new TradeOrder
+                {
+                    Id = existingOrder,
+                    CreatedDate = DateTime.Now,
+                    OrderType = "GTC",
+                    Price = 10,
+                    Quantity = 100,
+                    SecurityCode = "AAPL",
+                    Side = "SELL"
+                });
+
+            await context.SaveChangesAsync();
+
+            var service = new TradingService(context);
+
+            var orders = service.GetOrders();
+
+            orders
+                .Should()
+                .BeEquivalentTo(new[] { new ExecutedTradeOrder
+                {
+                    Id = existingOrder,
+                    CreatedDate = DateTime.Now,
+                    OrderType = "GTC",
+                    Price = 10,
+                    Quantity = 100,
+                    SecurityCode = "AAPL",
+                    Side = "SELL"
+                }}, options => options.Excluding(o => o.CreatedDate));
+        }
+
+        [Test]
+        public async Task ShouldNotRetrieveOrders()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var database = new DbContextOptionsBuilder<TradingDbContext>()
+                .UseInMemoryDatabase(databaseName: "tradeSystem")
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+
+            using var context = new TradingDbContext(database);
+
+            var existingOrder = Guid.NewGuid().ToString();
+            await context.TradeOrders.AddRangeAsync(
+                new TradeOrder
+                {
+                    Id = existingOrder,
+                    CreatedDate = DateTime.Now,
+                    OrderType = "GTC",
+                    Price = 10,
+                    Quantity = 0,
+                    SecurityCode = "AAPL",
+                    Side = "SELL"
+                });
+
+            await context.SaveChangesAsync();
+
+            var service = new TradingService(context);
+
+            var orders = service.GetOrders();
+
+            orders
+                .Should()
+                .BeEmpty();
         }
     }
 }
